2e1a5fb07b242084ea3728391eb5984933789544
Alex Tran <alextran0899@gmail.com>
2024-01-08 21:22:18 -0600

    add some incredible styling to home page

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -4,20 +4,40 @@ import axios from "axios";
 
 export default function Home() {
     const [images, setImages] = useState([]);
+    const divideImagesIntoColumn = (images) => {
+        let counter = 0;
+        if(images){
+            setImages(images.map(image => {
+                image.index = counter
+                counter = (counter + 1) % 4
+                return image
+            }))
+        }
+    }
 
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API_URL}/api/images`).then(
-            res => setImages(res.data)
+            res => divideImagesIntoColumn(res.data)
+
         ).catch(err => console.log(err));
     }, []);
 
-    const renderImage = (image) => {
-        return <img key={image.id} src={image.url} alt={`Image ${image.id}`} />;
-    };
 
+    const renderColumn = (col) => {
+        console.log(col)
+        if(images){
+            return (
+                <div className={style.column}>{images.map(each => each.index === col &&
+                    <img src={each.url} alt='img' className={style.column.img}/>
+                )}</div>)
+        }
+    }
     return (
-        <div>
-            {images.filter(image => image.url).map(renderImage)}
+        <div className={style.homePage}>
+            <div className={style.row}>
+                    {[...Array(4)].map((_, col) => renderColumn(col))}
+            </div>
+
         </div>
     );
 }
