33976ce4b7c79c05c153c7cdab444cb6402e02a0
Alex Tran <alextran0899@gmail.com>
2024-01-09 14:28:06 -0600

    add morphing menug

--- client/src/Home/ManageProfileCTA/HambugerMenu.js
+++ client/src/Home/ManageProfileCTA/HambugerMenu.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import style from "./HambugerMenu.module.css"
 import hamburgerMenuSVG from "../../svg-icon/hamburger-menu.svg"
 
 export default function HambugerMenu() {
+    const [showingMenu,setShowingMenu] = useState(false)
+
     return (
         <div className={style.menu}>
 
