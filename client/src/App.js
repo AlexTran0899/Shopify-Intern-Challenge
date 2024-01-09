307bef41cb3118f19a921c23d70eb2469710ae99
Alex Tran <alextran0899@gmail.com>
2024-01-08 19:55:34 -0600

    testing Link component for navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import Home from "./Home.js"
 import Admin from "./Admin";
-import {Route, Routes} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-      <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/admin" element={<Admin/>}/>
-      </Routes>
+      <div>
+          <Link to={'/admin'}>admin</Link>
+          <Link to={'/'}>home</Link>
+          <Routes>
+
+              <Route path="/" element={<Home/>}/>
+              <Route path="/admin" element={<Admin/>}/>
+          </Routes>
+      </div>
+
   );
 }
 
