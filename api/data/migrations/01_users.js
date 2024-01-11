5b7de0e3812261e7506867e7c1ce902e84e0eef0
Alex Tran <alextran0899@gmail.com>
2024-01-10 20:26:16 -0600

    adjust spacing a little

--- api/data/migrations/01_users.js
+++ api/data/migrations/01_users.js
@@ -1,18 +1,18 @@
 exports.up = async (knex) => {
   await knex.schema
     .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('email', 255).notNullable()
-      users.string('password', 255).notNullable()
-      users.string('img_url', 255)
-      users.string('address',255)
-      users.integer('zip',10)
-      users.string('state',2)
-      users.string('city',255)
-      users.integer('phone',20)
+        users.increments('user_id')
+        users.string('email', 255).notNullable()
+        users.string('password', 255).notNullable()
+        users.string('last_name')
+        users.string('first_name')
+        users.string('img_url', 255)
+        users.string('address',255)
+        users.integer('zip',10)
+        users.string('state',2)
+        users.string('city',255)
+        users.integer('phone',20)
       users.timestamps(false, true)
-      users.string('first_name')
-      users.string('last_name')
     })
 }
 
