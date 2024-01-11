32f0f84170e576fa0291ec04056d6f50a8b7a1c3
Alex Tran <alextran0899@gmail.com>
2024-01-11 12:12:02 -0600

    add nav button

--- client/src/Admin/AdminNavBar/AdminNavBar.js
+++ client/src/Admin/AdminNavBar/AdminNavBar.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import style from './AdminNavBar.module.css'
 import AdminSearchBar from "./AdminSearchBar/AdminSearchBar";
+import {useNavigate} from "react-router-dom";
 export default function AdminNavBar() {
+    const navigate = useNavigate()
+    const logout = () => {
+        localStorage.removeItem('email');
+        localStorage.removeItem('token');
+        setTimeout(() => navigate('/'), 500)
+    }
+
     return (
         <div className={style.body}>
+            {/*<button className={style.navBarButton}>Select Multiple</button>*/}
+            <button className={style.navBarButton}>Upload</button>
             <AdminSearchBar/>
+            <button onClick={logout} className={style.navBarButton}>Log Out</button>
         </div>
     )
 }
