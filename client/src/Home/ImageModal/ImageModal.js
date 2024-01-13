46e6f3e2521fec490bb7bfc633161574a7547da5
Alex Tran <alextran0899@gmail.com>
2024-01-13 15:18:36 -0600

    add styling to image modal

--- client/src/Home/ImageModal/ImageModal.js
+++ client/src/Home/ImageModal/ImageModal.js
@@ -12,9 +12,11 @@ export default function ImageModal({isShowingModal, selectedImage, setIsModalSho
             <div className={style.modalContent}>
                 <img src={selectedImage.url} alt={`image of ${selectedImage.image_title}`}/>
                 <h1>{selectedImage.image_title}</h1>
+                <p>Purchase original image</p>
                 <div className={style.footer}>
-
-                    {!originalImageLink && <div className={style.btn}><ApplePay item_info={selectedImage} setOriginalImageLink={setOriginalImageLink}/></div>}
+                    {!originalImageLink && <div className={style.btn}>
+                        <ApplePay item_info={selectedImage} setOriginalImageLink={setOriginalImageLink}/>
+                    </div>}
                     {originalImageLink && <a className={style.downloadButton} href={originalImageLink} download={selectedImage.image_title}><h1>DOWNLOAD IMAGE</h1></a>}
                     <CloseIconSVG className={style.closeButton} onClick={() => setIsModalShowing(false)}/>
                 </div>
--- client/src/Home/SearchBar/SearchBar.js
+++ client/src/Home/SearchBar/SearchBar.js
@@ -2,25 +2,47 @@ import React, {useState, useEffect} from 'react'
 import style from './SearchBar.module.css'
 export default function SearchBar() {
     const [searchText, setSearchText] = useState("")
-    const [placeHolderText, setPlaceHolderText] = useState("")
-    const fullPlaceholder = "Search for "; // Full placeholder text
+    const [placeHolderText, setPlaceHolderText] = useState(null)
+    const [isInputBoxFocus, setIsInputBoxFocus] = useState(false)
 
+    let isDeleting = false
+    const words = ["'car'", "'cave'", "'tree'", 'anything']
+    const wait = 2000
+    let txt = ''
+    let wordIndex = 0
+    const type = () => {
+        const current = wordIndex % words.length;
+        const fullTxt = words[current];
+        if (isDeleting) {
+            txt = fullTxt.substring(0, txt.length - 1);
+        } else {
+            txt = fullTxt.substring(0, txt.length + 1);
+        }
+        setPlaceHolderText(txt)
+        let typeSpeed = 100;
+        if (isDeleting) {
+            typeSpeed /= 2;
+        }
+        if (!isDeleting && txt === fullTxt) {
+            isDeleting = true
+            typeSpeed = wait;
+        } else if (isDeleting && txt === '') {
+            isDeleting = false
+            wordIndex++;
+            typeSpeed = 500;
+        }
+        setTimeout(() => type(), typeSpeed);
+    }
     useEffect(() => {
-        setInterval(() => {
-            setPlaceHolderText((prevText) => {
-                return fullPlaceholder.substr(0, prevText.length + 1);
-            });
-        }, 1000);
-
-    }, []);
-
+        type()
+    }, [])
 
     return (
         <div  className={style.searchBar}>
             <input type="text"
                    maxLength='255'
                    className={style.inputBox}
-                   placeholder={placeHolderText}
+                   placeholder={`Search for ${placeHolderText}`}
                    onChange={(e) => setSearchText(e.target.value)}
             />
         </div>
