5e12b65ea707d55a5957279e5c1de9c0237982c8
Alex Tran <alextran0899@gmail.com>
2024-01-13 15:52:59 -0600

    add image tittle to return for finding images

--- api/Image/image-model.js
+++ api/Image/image-model.js
@@ -26,7 +26,7 @@ async function find(char) {
     .andWhere({ public: 1 })
     .orWhere('tags', 'ilike', `%${char}%`)
     .andWhere({ public: 1 })
-    .select('url','price','image_key')
+    .select('url','price','image_key','image_title')
 }
 function findUserImage(user_id, char) {
   return db('image').where('image_title', 'ilike', `%${char}%`)
