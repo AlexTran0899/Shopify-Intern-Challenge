eed9027fc52ac6349ce08df7d5858148712b3298
Alex Tran <alextran0899@gmail.com>
2024-01-13 20:48:14 -0600

    fix image router pricing parsing issue

--- api/Image/image-router.js
+++ api/Image/image-router.js
@@ -33,9 +33,7 @@ router.get('/user-image', restricted, (req, res, next) => {
 })
 
 router.put('/:image_key', restricted, (req, res, next) => {
-    if(parseInt(req.body.price) < 0.50) { req.body.price = 0.50}
-    console.log(req.body.public)
-    console.log(req.body.public === true ? 1 : 0)
+    if(parseFloat(req.body.price) < 0.50) { req.body.price = 0.50}
     const new_data = {
         image_title:req.body.image_title,
         price:parseInt(req.body.price * 100),
