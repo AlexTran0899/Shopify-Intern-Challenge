12fb4c5b01199ec2c94fed5fd223798683fc245e
Alex Tran <alextran0899@gmail.com>
2024-01-14 23:32:54 -0600

    mission accomplish

--- client/src/Home/PhotoGrid/PhotoGrid.js
+++ client/src/Home/PhotoGrid/PhotoGrid.js
@@ -1,12 +1,14 @@
-import React, {useEffect, useState} from 'react'
-import style from './PhotoGrid.module.css'
+import React, { useEffect, useState } from 'react';
+import style from './PhotoGrid.module.css';
 
-export default function PhotoGrid({imageArray, openImageModalWithImage}) {
-    const [images, setImages] = useState([])
-    const assignIndex = (images) => {
+export default function PhotoGrid({ imageArray, openImageModalWithImage }) {
+    const [images, setImages] = useState([]);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    const assignIndex = (images, numColumns) => {
         if (images.length === 0) { return setImages([]); }
-        let columnHeights = [0, -300, -300, 0];
 
+        let columnHeights = [0,-300,-300,0];
         const index_assigned_images = images.map(image => {
             let minColumn = columnHeights.indexOf(Math.min(...columnHeights));
             image.column = minColumn;
@@ -18,27 +20,35 @@ export default function PhotoGrid({imageArray, openImageModalWithImage}) {
     }
 
     useEffect(() => {
-        assignIndex(imageArray)
-    }, [imageArray]);
+        // Function to handle screen resize
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        }
+
+        // Add resize event listener
+        window.addEventListener('resize', handleResize);
+
+        // Clean up
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+
+    useEffect(() => {
+        const numColumns = screenWidth >= 1000 ? 4 : 2;
+        assignIndex(imageArray, numColumns);
+    }, [imageArray, screenWidth]);
 
     return (
         <div className={style.row}>
-            <div className={style.column}>
-                {images && images.filter((image) => image.column === 0).map(image =>
-                    <img key={image.image_key} src={image.url} alt='img' onClick={() => openImageModalWithImage(image)}/>)}
-            </div>
-            <div className={style.column}>
-                {images && images.filter((image) => image.column === 1).map(image =>
-                    <img key={image.image_key} src={image.url} alt='img' onClick={() => openImageModalWithImage(image)}/>)}
-            </div>
-            <div className={style.column}>
-                {images && images.filter((image) => image.column === 2).map(image =>
-                    <img key={image.image_key} src={image.url} alt='img' onClick={() => openImageModalWithImage(image)}/>)}
-            </div>
-            <div className={style.column}>
-                {images && images.filter((image) => image.column === 3).map(image =>
-                    <img key={image.image_key} src={image.url} alt='img' onClick={() => openImageModalWithImage(image)}/>)}
-            </div>
+            {[...Array(screenWidth >= 1000 ? 4 : 2)].map((_, colIndex) => (
+                <div key={colIndex} className={`${screenWidth >= 1000 ? `${style.column4}` : `${style.column2}`}`}>
+                    {console.log()}
+                    {images && images.filter((image) => image.column === colIndex).map(image => (
+                        <img key={image.image_key} src={image.url} alt='img' onClick={() => openImageModalWithImage(image)} />
+                    ))}
+                </div>
+            ))}
         </div>
-    )
+    );
 }
