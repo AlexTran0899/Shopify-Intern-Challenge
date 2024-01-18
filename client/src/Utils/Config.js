0b51f6c30aad513b585fd81f25ab22c6b7c48b86
Alex Tran <alextran0899@gmail.com>
2024-01-17 23:09:17 -0600

    add dynamic check and set for REACT_APP_API_URL

--- client/src/Utils/Config.js
+++ client/src/Utils/Config.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-    REACT_APP_API_URL:'https://shopify-intern-challenge-production.up.railway.app'
+    REACT_APP_API_URL: process.env.REACT_APP_API_URL || 'https://shopify-intern-challenge-production.up.railway.app'
 }
